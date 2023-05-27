6a7ac0f9fde598da1bec6799ecffcebc41b144bc
slhmy <1484836413@qq.com>
2023-05-27 09:57:14 +0800

    Spawn kitchenObjects on counters

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -3,7 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour {
+    
+    [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private Transform kitchenObject;
+    private KitchenObject _kitchenObject;
+
     public void Interact() {
         Debug.Log("ClearCounter.Interact()");
+        if (!HasKitchenObject()) {
+            KitchenObject.SpawnKitchenObject(kitchenObject, counterTopPoint);
+        }
+    }
+    
+    private bool HasKitchenObject() {
+        return _kitchenObject != null;
     }
 }
---
+++ Assets/Scripts/KitchenObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchenObject : MonoBehaviour {
+    public static void SpawnKitchenObject(Transform spawningKitchenObject, Transform counterTopPoint) {
+        Transform kitchenObjectTransform = Instantiate(spawningKitchenObject, counterTopPoint, true);
+        kitchenObjectTransform.localPosition = Vector3.zero;
+    }
+}
